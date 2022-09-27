2769e9e643f4fa4ed28713482d6225d613ffcfa6
Your Name <you@example.com>
2022-09-27 14:26:55 -0500

    Created Database in MySQL with PyCharm

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+
+
+
+#Callout section
+connection = create_server_connection("localhost", "root", "student")
+database_query = "CREATE DATABASE United_Air"
+create_database(connection, database_query)
+
